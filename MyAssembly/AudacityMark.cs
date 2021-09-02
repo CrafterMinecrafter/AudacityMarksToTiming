3ce83645967d26377265fd820baa91e0ddd97f61
Даниил <67530305+CrafterMinecrafter@users.noreply.github.com>
2021-09-02 19:48:33 +0200

    Добавьте файлы проекта.

---
+++ MyAssembly/AudacityMark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class AudacityMark
+{
+    public AudacityMark(string data)
+    {
+        string[] parsed_data = data.Replace('.', ',').Replace('\t', ' ').Split(new char[]
+        {
+            ' '
+        });
+        StartMS = Convert.ToInt32(double.Parse(parsed_data[0]) * 1000.0);
+
+        EndMS = Convert.ToInt32(double.Parse(parsed_data[1]) * 1000.0);
+    }
+
+    public int intervalMS
+    {
+        get => EndMS - StartMS;
+    }
+
+    public int StartMS;
+
+    public int EndMS;
+}
---
+++ MyAssembly/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    public static void Main(string[] MyFile)
+    {
+        if (MyFile == null)
+        {
+            Console.WriteLine("pls put file with AudacityMarks in exe");
+            return;
+        }
+
+        string directoryName = Path.GetDirectoryName(MyFile[0]);
+        string[] dataLines = File.ReadAllLines(MyFile[0]);
+
+        List<AudacityMark> audacityMarksList = new List<AudacityMark>();
+        foreach (string data in dataLines)
+        {
+            audacityMarksList.Add(new AudacityMark(data));
+        }
+
+        string[] contents = ACMarksToOsuTiming(audacityMarksList.ToArray());
+        File.WriteAllLines(Path.Combine(directoryName, "Timing.txt"), contents);
+    }
+
+    public static string[] ACMarksToOsuTiming(AudacityMark[] marks)
+    {
+        List<string> list = new List<string>();
+        foreach (AudacityMark audacityMark in marks)
+        {
+            list.Add($"{audacityMark.StartMS},{audacityMark.intervalMS * 2},1,2,100,100,1,0");
+        }
+        return list.ToArray();
+    }
+}
